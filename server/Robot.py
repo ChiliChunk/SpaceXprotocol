e1a23cfc469081c98ff0760bb4d028e47f90cedb
Nicolas <fussure@gmail.com>
2019-04-07 16:32:14 +0200

    add lot of stuff

--- server/Robot.py
+++ server/Robot.py
@@ -6,5 +6,12 @@ class Robot:
         self.ressources = {}
         self.isPaused = False
 
+    def addRessources (self,ressources):
+        for res in ressources:
+            if res in self.ressources:
+                self.ressources[res] += 1
+            else:
+                self.ressources[res] = 1
+
     def __str__(self):
-        return f"{self.name} {'PAUSED' if self.isPaused else 'In_Acivity'} ({self.x},{self.y})"
+        return f"{self.name} {'PAUSED' if self.isPaused else 'In_Acivity'} ({self.x},{self.y}) {self.ressources}"
--- server/server.py
+++ server/server.py
@@ -7,34 +7,84 @@ import os.path
 import Robot
 from random import randint
 
-lockName = threading.Semaphore()
+lockRobots = threading.Semaphore()
+lockCells = threading.Semaphore()
 mapPseudo = {}
 cellsWithRessources = {}
 
 
 def addUser(pseudo, socket):
-    lockName.acquire()
+    lockRobots.acquire()
     mapPseudo[socket] = Robot.Robot(pseudo)
-    lockName.release()
+    lockRobots.release()
     return {"code": 200}
 
 
 def removeUser(socket):
-    lockName.acquire()
+    lockRobots.acquire()
     del mapPseudo[socket]
-    lockName.release()
+    lockRobots.release()
     return {"code": 200}
 
-def mapMessage(row,col):
+def mapMessage(row,col , socket):
     print(row,col)
     result = {}
+    result["dimension"] = [col,row]
+    robotList = []
+    for sock , robot in mapPseudo.items():
+        if sock != socket:
+            robotList.append({"x":robot.x , "y":robot.y , "name" : robot.name})
+    result["robots"] = robotList
+    selfRobot = mapPseudo[socket]
+    result["self"] = {"x":selfRobot.x , "y":selfRobot.y , "name":selfRobot.name}
+    result["code"] = 200
+    print(result)
     return result
 
+def setPaused(socket , isPaused):
+    with lockRobots:
+        mapPseudo[socket].isPaused = isPaused
+        print (mapPseudo[socket])
+        return {"code" : 200}
 
 def writeLogLine(fileName, pseudo, command, code):
     with open(fileName, "a") as f:
         f.write(f"{round(time.time())}   {pseudo}   {command}   {code}\n")
 
+def changeName(socket , newName):
+    with lockRobots:
+        mapPseudo[socket].name = newName
+
+def setPosition(socket , x ,y, maxX , maxY):
+    if x >= int(maxX) or x < 0 or y >= int(maxY) or y < 0: #placement outside of the grid
+        return {"code": 411}
+    with lockRobots:
+        for robot in mapPseudo.values():
+            if robot.x == x and robot.y ==y: #try placement on another robot
+                return {"code" : 411}
+        mapPseudo[socket].x = x
+        mapPseudo[socket].y = y
+        mapPseudo[socket].addRessources(getRessources(x,y))
+        print(mapPseudo[socket])
+        return {"code" : 200}
+
+
+
+def getRessources (x , y):
+    with lockCells:
+        if (x,y) in cellsWithRessources:
+            temp = cellsWithRessources[(x,y)]
+            del cellsWithRessources[(x,y)]
+            return temp
+        else:
+            return []
+
+
+def placement (socket, x,y, maxX , maxY):
+    if mapPseudo[socket].x != None or mapPseudo[socket].y != None:
+        return {"code" : 405}
+    return setPosition(socket, x,y, maxX , maxY)
+        
 
 def traiter_client(socket_client, conf):
     connected = True
@@ -59,8 +109,15 @@ def traiter_client(socket_client, conf):
                 connected = False
                 message = removeUser(sock_client)
             if jsonData["exchange"] == "map":
-                message = mapMessage(conf["map_number_row"] , conf["map_number_col"])
+                message = mapMessage(conf["map_number_row"] , conf["map_number_col"] , sock_client)
+            if jsonData["exchange"] == "pause" or jsonData ["exchange"] == "continue":
+                message = setPaused(sock_client ,jsonData["exchange"] == "pause")
+            if jsonData["exchange"] == "changeName":
+                message = changeName(socket_client , jsonData["pseudo"])
+            if jsonData["exchange"] =="placement":
+                message = placement(socket_client,jsonData["data"][0] , jsonData["data"][1] , conf["map_number_col"] , conf["map_number_col"])
 
+            print(message)
             writeLogLine(conf["logs"], mapPseudo[sock_client].name, jsonData["exchange"],
                          message["code"])  # FIXME pseudo undefined pour le logout
         else:
@@ -68,6 +125,7 @@ def traiter_client(socket_client, conf):
                 writeLogLine(conf["logs"],
                              mapPseudo[sock_client].name if sock_client in mapPseudo.keys() else "unknown",
                              "BAD_FORMAT", message["code"])  # FIXME pseudo undefined pour le logout
+                                #TODO f'{socket_client.raddr[0]}:{socket_client.raddr[1] a la place de unknown
         socket_client.send((json.dumps(message) + "\n").encode())
 
 
@@ -83,7 +141,6 @@ def loadConfiguration():
 
 
 def createMap(row, col, ressources):
-    print(row, col)
     result = {}
     ressourcesDict = json.loads(ressources)
     for i in range(row):
@@ -99,6 +156,8 @@ def createMap(row, col, ressources):
 if __name__ == '__main__':
     conf = loadConfiguration()
     cellsWithRessources = createMap(int(conf['map_number_row']), int(conf['map_number_col']), conf["ressources"])
+    print(cellsWithRessources)
+    print(getRessources(1,1))
     sock_server = socket()  # TCP socket
     sock_server.bind(("", int(conf['port'])))
     print(f"Server listening on port : {conf['port']}")
